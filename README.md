1ea4dc1160a6f361e264b82e07459d2313f057d9
YorlenisArroyo <55029650+YorlenisArroyo@users.noreply.github.com>
2020-06-14 23:39:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simuladortranque
+ SimPy
---
+++ Tranque.py
@@ -0,0 +1,25 @@
+import simpy
+
+def tranque(env):
+    while True:
+        print('Despejado %d ' % env.now)
+        despejado = 3
+        yield env.timeout(despejado)
+
+        print('Tranque en la Tarde! por %d horas, luego estara despejado  ' % env.now)
+        duracion_mañana = 1
+        yield env.timeout(duracion_mañana)
+
+        print('Tranque en la Noche! por %d horas, luego estara despejado  ' % env.now)
+        duracion_tarde = 1
+        yield env.timeout(duracion_tarde)
+
+        print('Tranque en la manana! por %d horas, luego estara despejado  '% env.now)
+        yield env.timeout(4)
+
+# Creamos nuestro Entorno
+env = simpy.Environment()
+# Lo agregamos la función de semaforo, al Proceso del Entorno
+env.process(tranque(env))
+# Ejecutamos agregando el tiempo de 15 Segundo
+env.run(until=6)
